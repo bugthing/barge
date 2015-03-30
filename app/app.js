815654d3282ab37af726270606be18f208fa5e57
Benjamin Martin <benjamin247365@hotmail.com>
2015-03-30 22:59:21 +0100

    Render my first componant

--- app/app.js
+++ app/app.js
@@ -1,12 +1,14 @@
 import LocalStorage from 'localStorage'
-import React from 'react'
+import React from 'react/addons'
 import Css from './style.css'
 import Content from './content'
+
+import DockerHost from './components/DockerHost'
 
 var dockerHost = LocalStorage.getItem('dockerHost')
 
 if(dockerHost) {
   document.write( Content )
 } else {
-  document.write( 'Get docker host' )
+  React.render(<DockerHost />, document.body);
 }
---
+++ app/components/DockerHost.js
@@ -0,0 +1,8 @@
+
+import React from 'react/addons'
+export default React.createClass({
+    displayName: 'DockerHost',
+    render: () => {
+        return <div>Enter the docker host here: <input /></div>
+    }
+})
