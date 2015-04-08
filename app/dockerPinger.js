e8e718336a6f99489cfa854404136e948f0c4fd7
Benjamin Martin <benjamin247365@hotmail.com>
2015-04-08 22:45:28 +0100

    started to use http module

--- app/dockerPinger.js
+++ app/dockerPinger.js
@@ -1,17 +1,29 @@
 
-// TODO!- why you no work!
-
-//import request from 'request'
+import http from 'http'
 
 export default function(host) {
     return new Promise( (resolve, reject) => {
-      //request(host, function (error, response, body) {
-      //   if (!error && response.statusCode == 200) {
-         if (true) {
-            resolve(host)
-         } else {
-            reject(false)
-         }
-      //})
+
+        var options = {
+            protocol: 'http:',
+            hostname: host,
+            port: 4375,
+            path: '/_ping',
+            headers: {
+                'Origin': 'http://localhost'
+            }
+        };
+
+        http.get(options, function (res) {
+            res.on("end", function () {
+                if(res.statusCode == 200) {
+                    console.log('GOOD')
+                    resolve(host)
+                } else {
+                    console.log('BAD')
+                    reject()
+                }
+            });
+        });
     })
 }
